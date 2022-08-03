9a212e386c83bc28140c0bcb7b6430b3acbbb90a
EstiLev <st3281357@gmail.com>
2022-08-03 11:02:55 +0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule,MongooseModule.forRoot('mongodb://localhost/nest')],
+ 
 })
 export class AppModule {}
